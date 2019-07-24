1886624f79ecd0fabab7d63883c09212d92e718f
Marc Melcher <marcdavidmelcher@gmail.com>
2019-07-24 12:52:38 -0500

    remove outdated info from pom

---
+++ src/main/java/com/revature/login.java
@@ -0,0 +1,89 @@
+package com.revature;
+
+import com.revature.dao.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import java.util.Scanner;
+
+
+
+public class login extends HttpServlet {
+
+	private static final long serialVersionUID = -1641096228274971485L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+				
+			
+		// set response headers
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		// create HTML form
+		PrintWriter writer = response.getWriter();
+		writer.append("<!DOCTYPE html>\r\n")
+			  .append("<html>\r\n")
+			  .append("		<head>\r\n")
+			  .append("			<title>login</title>\r\n")
+			  .append("		</head>\r\n")
+			  .append("		<body>\r\n")
+			  .append("			<form action=\"dfa\" method=\"POST\">\r\n")
+			  .append("				name: \r\n")
+			  
+			  .append("				<input type=\"text\" name=\"user\" />\r\n")
+			  .append("				password: \r\n")
+			  .append("				<input type=\"password\" name=\"password\" />\r\n")
+			  .append("				<input type=\"submit\" value=\"Submit\" />\r\n")
+			  .append("			</form>\r\n")
+			  .append("		</body>\r\n")
+			  .append("</html>\r\n");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+				String user = request.getParameter("user");
+				String password = request.getParameter("password");
+				DataManager session = new DataManager();
+				session.connect();
+				session.register(user,password);
+				String test = session.say();
+
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		// create HTML response
+		PrintWriter writer = response.getWriter();
+		writer.append("<!DOCTYPE html>\r\n")
+			  .append("<html>\r\n")
+			  .append("		<head>\r\n")
+			  .append("			<title>logged in</title>\r\n")
+			  .append("		</head>\r\n")
+			  .append("		<body>\r\n");
+
+			  
+		if (user != null && !user.trim().isEmpty()) {
+			writer.append("	Welcome " + user + ".\r\n");
+			writer.append("	You wrote a name.\r\n");
+		} else {
+			writer.append("	You did not entered a name!\r\n");
+		}
+		writer.append("		</body>\r\n")
+			  .append("</html>\r\n");
+	}	
+	
+
+	public void destroy(  ) {
+		
+	  }
+	  
+
+}
