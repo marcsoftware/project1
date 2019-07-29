43dac6f57365cbe7afa8b1bfcfc99ada0d5ded3c
Marc Melcher <marcdavidmelcher@gmail.com>
2019-07-29 11:55:19 -0500

    added homepage & user is redirected to homepage after logging in.

--- src/main/java/com/revature/homepage.java
+++ src/main/java/com/revature/homepage.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 
 
-public class login extends HttpServlet {
+public class homepage extends HttpServlet {
 
 	private static final long serialVersionUID = -1641096228274971485L;
 
@@ -43,57 +43,13 @@ public class login extends HttpServlet {
 			  .append("		</head>\r\n")
 			  .append("		<body>\r\n")
 			  .append("			<p>:::"+user.getValue( )+"</p>\r\n")
-			  .append("		login:\n\r")
-			  .append("			<form action=\"login\" method=\"POST\">\r\n")
-			  .append("				name: \r\n")
-			  
-			  .append("				<input type=\"text\" name=\"user\" />\r\n")
-			  .append("				password: \r\n")
-			  .append("				<input type=\"password\" name=\"password\" />\r\n")
-			  .append("				<input type=\"submit\" value=\"Submit\" />\r\n")
-			  .append("			</form>\r\n")
+			  .append("			<p>this is the homepage</p>\r\n")
+	
 			  .append("		</body>\r\n")
 			  .append("</html>\r\n");
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-				String user = request.getParameter("user");
-				String password = request.getParameter("password");
-				DataManager session = new DataManager();
-				session.connect();
-				
-				Cookie test = new Cookie("user", user);
-				test.setMaxAge(60*60*24);
-				response.addCookie( test );
-				 test = new Cookie("password", password);
-				test.setMaxAge(60*60*24);
-				response.addCookie( test );
-		response.setContentType("text/html");
-		response.setCharacterEncoding("UTF-8");
-		
-		// create HTML response
-		PrintWriter writer = response.getWriter();
-		writer.append("<!DOCTYPE html>\r\n")
-			  .append("<html>\r\n")
-			  .append("		<head>\r\n")
-			  .append("			<title>logged in</title>\r\n")
-			  .append("		</head>\r\n")
-			  .append("		<body>\r\n");
 
-			  
-		if (user != null && !user.trim().isEmpty()) {
-			writer.append("	Welcome " + user + ".\r\n");
-			writer.append("	----You are logged in.\r\n");
-		//	createSession( request,  response);
-			response.sendRedirect("/app/homepage");  
-		} else {
-			writer.append("	some went wrong.\r\n");
-		}
-		writer.append("		</body>\r\n")
-			  .append("</html>\r\n");
-	}	
 	
 
 	public void destroy(  ) {
@@ -107,12 +63,12 @@ public class login extends HttpServlet {
 			PrintWriter out = response.getWriter();  
 			
 			String n=request.getParameter("user");  
-			out.print(" Welcome "+n);  
+			out.print("Welcome "+n);  
 			
 			HttpSession session=request.getSession();  
 			session.setAttribute("uname",n);  
 	
-			out.print(" <a href='/app/homepage'>homepage</a> ");  
+			out.print("<a href='/app/request'>apply</a>");  
 					
 			out.close();  
   
