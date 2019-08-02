3bb1bc90ff39474e18aaae1dd545733a8c116340
Marc Melcher <marcdavidmelcher@gmail.com>
2019-08-02 06:56:52 -0500

    picture was uploaded and rendered.

--- src/main/java/com/revature/view.java
+++ src/main/java/com/revature/view.java
@@ -56,7 +56,7 @@ public class view extends HttpServlet {
 			  .append("		<head>\r\n")
 			  
 			  .append("			<title>view</title>\r\n")
-			  .append("<style>img{width:10%};#customers { font-family: 'Trebuchet MS, Arial, Helvetica, sans-serif'; border-collapse: collapse; width: 100%; } #customers td, #customers th { border: 1px solid #ddd; padding: 8px; width:10%} #customers tr:nth-child(even){background-color: #f2f2f2;} #customers tr:hover {background-color: #ddd;} #customers th { padding-top: 12px; padding-bottom: 12px; text-align: left; background-color: #4CAF50; color: white; }</style>\r\n")
+			  .append("<style>img{width:20%;}#customers { font-family: 'Trebuchet MS, Arial, Helvetica, sans-serif'; border-collapse: collapse; width: 100%; } #customers td, #customers th { border: 1px solid #ddd; padding: 8px; width:10%} #customers tr:nth-child(even){background-color: #f2f2f2;} #customers tr:hover {background-color: #ddd;} #customers th { padding-top: 12px; padding-bottom: 12px; text-align: left; background-color: #4CAF50; color: white; }</style>\r\n")
 			  .append("		</head>\r\n")
 			  .append("		<body>\r\n")
 			  .append("			<p>"+user.getValue( )+"</p> view all\r\n");
@@ -79,12 +79,14 @@ public class view extends HttpServlet {
 			writer.append("<tr>");
 			writer.append("			<td id='cell'>"+result.get(i)+"</td>");
 			writer.append("			<td id='cell'>"+result.get(i+1)+"</td>");
-			filePath="http://localhost:8080/app/img/";
+			
+			
 			String img_name=result.get(i+2);
+			String img_url= "<img src='http://localhost:8080/app/data/"+img_name+".png'>";
 			if(img_name.equals("")){
-
+				img_url="";
 			}
-			writer.append("			<td id='cell'><img src='"+filePath+"1.png'></td>");
+			writer.append("			<td id='cell'>"+img_url+"</td>");
 			writer.append("			<td id='cell'>"+result.get(i+3)+"</td>");
 			writer.append("			<td id='cell'>"+result.get(i+4)+"</td>");
 			writer.append("</tr>");
